b647179fd38925513ece05acb41bc6a4411dc2c3
allenfataki <oo24514@gmail.com>
2019-07-30 10:40:11 +0800

    make msmt17 data structure same as market1501

--- data/data_manager.py
+++ data/data_manager.py
@@ -657,28 +657,22 @@ class MSMT17(object):
     """
     dataset_dir = 'msmt17'
 
-    def __init__(self, root='data', **kwargs):
+    def __init__(self, cfg, **kwargs):
+        root = cfg.DATASET.TRAIN_PATH
         self.dataset_dir = osp.join(root, self.dataset_dir)
-        self.train_dir = osp.join(self.dataset_dir, 'MSMT17_V1/train')
-        self.test_dir = osp.join(self.dataset_dir, 'MSMT17_V1/test')
-        self.list_train_path = osp.join(self.dataset_dir, 'MSMT17_V1/list_train.txt')
-        self.list_val_path = osp.join(self.dataset_dir, 'MSMT17_V1/list_val.txt')
-        self.list_query_path = osp.join(self.dataset_dir, 'MSMT17_V1/list_query.txt')
-        self.list_gallery_path = osp.join(self.dataset_dir, 'MSMT17_V1/list_gallery.txt')
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
 
         self._check_before_run()
-        train, num_train_pids, num_train_imgs = self._process_dir(self.train_dir, self.list_train_path)
-        #val, num_val_pids, num_val_imgs = self._process_dir(self.train_dir, self.list_val_path)
-        query, num_query_pids, num_query_imgs = self._process_dir(self.test_dir, self.list_query_path)
-        gallery, num_gallery_pids, num_gallery_imgs = self._process_dir(self.test_dir, self.list_gallery_path)
 
-        #train += val
-        #num_train_imgs += num_val_imgs
-
+        train, num_train_pids, num_train_imgs = self._process_dir(self.train_dir, relabel=True)
+        query, num_query_pids, num_query_imgs = self._process_dir(self.query_dir, relabel=False)
+        gallery, num_gallery_pids, num_gallery_imgs = self._process_dir(self.gallery_dir, relabel=False)
         num_total_pids = num_train_pids + num_query_pids
         num_total_imgs = num_train_imgs + num_query_imgs + num_gallery_imgs
 
-        print("=> MSMT17 loaded")
+        print("=> Market1501 loaded")
         print("Dataset statistics:")
         print("  ------------------------------")
         print("  subset   | # ids | # images")
@@ -704,26 +698,34 @@ class MSMT17(object):
             raise RuntimeError("'{}' is not available".format(self.dataset_dir))
         if not osp.exists(self.train_dir):
             raise RuntimeError("'{}' is not available".format(self.train_dir))
-        if not osp.exists(self.test_dir):
-            raise RuntimeError("'{}' is not available".format(self.test_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
 
-    def _process_dir(self, dir_path, list_path):
-        with open(list_path, 'r') as txt:
-            lines = txt.readlines()
-        dataset = []
+    def _process_dir(self, dir_path, relabel=False):
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
         pid_container = set()
-        for img_idx, img_info in enumerate(lines):
-            img_path, pid = img_info.split(' ')
-            pid = int(pid) # no need to relabel
-            camid = int(img_path.split('_')[2])
-            img_path = osp.join(dir_path, img_path)
-            dataset.append((img_path, pid, camid))
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
             pid_container.add(pid)
-        num_imgs = len(dataset)
+        pid2label = {pid:label for label, pid in enumerate(pid_container)}
+
+        dataset = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1 # index starts from 0
+            if relabel: pid = pid2label[pid]
+            dataset.append((img_path, pid, camid))
+
         num_pids = len(pid_container)
-        # check if pid starts from 0 and increments with 1
-        for idx, pid in enumerate(pid_container):
-            assert idx == pid, "See code comment for explanation"
+        num_imgs = len(dataset)
         return dataset, num_pids, num_imgs
 
 """Video ReID"""
