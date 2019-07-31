f0c99e3ca9179a8d9a189fd936e037b2ed49b533
allenfataki <oo24514@gmail.com>
2019-07-31 11:47:18 +0800

    add dataset

--- data/data_manager.py
+++ data/data_manager.py
@@ -728,6 +728,83 @@ class MSMT17(object):
         num_imgs = len(dataset)
         return dataset, num_pids, num_imgs
 
+class MSMT17_TOTAL(object):
+    """
+    MSMT17
+
+    Reference:
+    Wei et al. Person Transfer GAN to Bridge Domain Gap for Person Re-Identification. CVPR 2018.
+
+    URL: http://www.pkuvmc.com/publications/msmt17.html
+    
+    Dataset statistics:
+    # identities: 4101
+    # images: 32621 (train) + 11659 (query) + 82161 (gallery)
+    # cameras: 15
+    """
+    dataset_dir = 'msmt17'
+
+    def __init__(self, cfg, **kwargs):
+        root = cfg.DATASET.TRAIN_PATH
+        self.dataset_dir = osp.join(root, self.dataset_dir)
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
+
+        self._check_before_run()
+
+        train, num_train_pids, num_train_imgs = self._process_dir([self.train_dir, self.query_dir, self.gallery_dir], relabel=True)
+
+        logger.info("=> MSMT17 loaded")
+        logger.info("Dataset statistics:")
+        logger.info("  ------------------------------")
+        logger.info("  subset   | # ids | # images")
+        logger.info("  ------------------------------")
+        logger.info("  train    | {:5d} | {:8d}".format(num_train_pids, num_train_imgs))
+        logger.info("  ------------------------------")
+
+        self.train = train
+
+        self.num_train_pids = num_train_pids
+
+    def _check_before_run(self):
+        """Check if all files are available before going deeper"""
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+
+    def _process_dir(self, dir_paths, relabel=False):
+        img_paths = []
+        for dir_path in dir_paths:
+            _img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+            img_paths.extend(_img_paths)
+
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        pid_container = set()
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid:label for label, pid in enumerate(pid_container)}
+
+        dataset = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            camid -= 1 # index starts from 0
+            if relabel: pid = pid2label[pid]
+            dataset.append((img_path, pid, camid))
+
+        num_pids = len(pid_container)
+        num_imgs = len(dataset)
+        return dataset, num_pids, num_imgs
+
 """Video ReID"""
 
 class Mars(object):
@@ -1280,6 +1357,7 @@ __img_factory = {
     'cuhk03': CUHK03,
     'dukemtmcreid': DukeMTMCreID,
     'msmt17': MSMT17,
+    'msmt17_total': MSMT17_TOTAL
 }
 
 __vid_factory = {
