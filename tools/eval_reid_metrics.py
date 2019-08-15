b441a04f07f2e583c7511e2b2fb8b315ddb64baf
allenfutaki <oo24514@gmail.com>
2019-08-15 11:14:19 +0800

    add recall and plot

--- tools/eval_reid_metrics.py
+++ tools/eval_reid_metrics.py
@@ -4,6 +4,9 @@ import copy
 from collections import defaultdict
 import sys
 from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def eval_cuhk03(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, N=100):
     """Evaluation with cuhk03 metric
@@ -37,7 +40,7 @@ def eval_cuhk03(distmat, q_pids, g_pids, q_camids, g_camids, max_rank, N=100):
             # this condition is true when query identity does not appear in gallery
             continue
 
-        kept_g_pids = g_pids[order][keep]
+        kept_g_pids = g_pids[order][keep] 
         g_pids_dict = defaultdict(list)
         for idx, pid in enumerate(kept_g_pids):
             g_pids_dict[pid].append(idx)
@@ -88,6 +91,7 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
     all_cmc = []
     all_AP = []
     num_valid_q = 0. # number of valid query
+
     for q_idx in tqdm(range(num_q), desc="CMC"):
         # get query pid and camid
         q_pid = q_pids[q_idx]
@@ -100,6 +104,7 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
 
         # compute cmc curve
         orig_cmc = matches[q_idx][keep] # binary vector, positions with value 1 are correct matches
+
         if not np.any(orig_cmc):
             # this condition is true when query identity does not appear in gallery
             continue
@@ -127,8 +132,80 @@ def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
 
     return all_cmc, mAP
 
+def eval_recall(distmat, q_pids, g_pids, q_camids, g_camids, save=False, name="recall"):
+    """Evaluation with market1501 metric
+    Key: for each query identity, its gallery images from the same camera view are discarded.
+    """
+    num_q, num_g = distmat.shape
+    indices = np.tile(g_pids, (q_pids.shape[0],1))
+    matches = (indices == q_pids[:, np.newaxis]).astype(np.int32)
+    confmat = 1 - distmat
+
+    # compute cmc curve for each query
+    recalls = []
+    precisions = []
+    for thresh in tqdm(range(0,101,1), desc="Recall"):
+        recall = []
+        precision = []
+        thresh *= 0.01
+        for q_idx in range(num_q):
+            # get query pid and camid
+            q_pid = q_pids[q_idx]
+            q_camid = q_camids[q_idx]
+
+            # remove gallery samples that have the same pid and camid with query        
+            remove = (g_pids == q_pid) & (g_camids == q_camid)
+            keep = np.invert(remove)
+
+            # compute number of true positive and positive given query
+            TP = np.sum(confmat[q_idx][keep][matches[q_idx][keep]==1] >= thresh, dtype=np.float32)
+            PP = np.sum(confmat[q_idx][keep] >= thresh, dtype=np.float32)
+            P = np.sum(matches[q_idx][keep], dtype=np.float32)
+            recall.append(TP/P)
+            precision.append(TP/PP if PP != 0.0 else 0.0)
+
+        recall = np.array(recall)
+        recalls.append(np.mean(recall))
+        precision = np.array(precision)
+        precisions.append(np.mean(precision))
+
+    thresh = np.array(list(range(0,101,1)))*0.01
+    recalls = np.array(recalls)
+    precisions = np.array(precisions)
+    df = pd.DataFrame({'thresh': thresh, 'recall': recalls, 'precision': precisions})    
+    df.to_csv("./{}.csv".format(name), index=False)
+
+    if save:                
+        plt.figure(figsize=(12, 5))
+        sns.set()
+        sns.lineplot(x="thresh", y="recall", data=df, color="b", label="recall")
+        sns.lineplot(x="thresh", y="precision", data=df, color="r", label="precision")
+        for i in range(0,101,10):
+            i *= 0.01
+            offset = -0.1 if i <= 0.5 else 0.1
+            r_value = df[df['thresh']==i]['recall'].iloc[0]
+            plt.text(x=i, y=r_value+offset, s="{:.3f}".format(r_value), color="b")
+            plt.scatter(i, r_value, s=50, c="b")
+            p_value = df[df['thresh']==i]['precision'].iloc[0]
+            plt.text(x=i, y=r_value+2*offset, s="{:.3f}".format(p_value), color="r")
+            plt.scatter(i, p_value, s=50, c="r")
+        plt.xlabel("Threshold")
+        plt.ylabel("Value")
+        plt.legend(bbox_to_anchor=(0.85, 0.95), loc='upper left', borderaxespad=0.5)
+        plt.title("{} => {}".format(name.split("_")[3], name.split("_")[1]))
+        plt.savefig("{}.jpg".format(name))
+        
+
 def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50, use_metric_cuhk03=False):
     if use_metric_cuhk03:
         return eval_cuhk03(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
     else:
         return eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank)
+
+if __name__ == '__main__':
+    distmat = np.load("./dismat.npy")
+    q_pids = np.load("./q_pids.npy")
+    g_pids = np.load("./g_pids.npy")
+    q_camids = np.load("./q_camids.npy")
+    g_camids = np.load("./g_camids.npy")
+    evaluate(distmat, q_pids, g_pids, q_camids, g_camids)
