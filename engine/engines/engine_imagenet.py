0786ba8121467c55399d26b15c5a6a650312d1a6
allenfutaki <oo24514@gmail.com>
2019-10-17 16:16:46 +0800

    remove data prefetcher

--- engine/engines/engine_imagenet.py
+++ engine/engines/engine_imagenet.py
@@ -12,7 +12,7 @@ logger = logging.getLogger("logger")
 class ImageNetEngine(Engine):
     def __init__(self, cfg, opts, tdata, vdata, show, manager):
         super(ImageNetEngine, self).__init__(cfg, opts, tdata, vdata, None, None, show, manager)
-        self.prefetcher = data_prefetcher(self.tdata)
+
     def _train_iter_start(self):
         self.iter += 1
         for opt in self.opts:
@@ -31,12 +31,11 @@ class ImageNetEngine(Engine):
             self.show.add_scalar('train/opt/{}/lr'.format(i), self.opts[i].monitor_lr, self.iter) 
 
     def _train_once(self):
-        for _ in tqdm(range(len(self.tdata)), desc="Epoch[{}/{}]".format(self.epoch, self.max_epoch)):
+        for batch in tqdm(self.tdata), desc="Epoch[{}/{}]".format(self.epoch, self.max_epoch)):
             self._train_iter_start()
 
-            # images, target = batch
-            # if self.use_gpu: images, target = images.cuda(), target.cuda()
-            images, target = self.prefetcher.next()
+            images, target = batch
+            if self.use_gpu: images, target = images.cuda(), target.cuda()
             
             outputs = self.core(images)
 
