13fc8a3671cb96b2ebdba6bb0ff2470e0bf99269
allenfutaki <oo24514@gmail.com>
2019-11-04 14:28:24 +0800

    add dataset

--- data/data_manager.py
+++ data/data_manager.py
@@ -19,6 +19,84 @@ logger = logging.getLogger("logger")
 
 from tools.utils import mkdir_if_missing, write_json, read_json
 
+class SOGO(object):
+    """
+    SOGO
+    
+    Dataset statistics:
+    # identities: ???
+    # images: 5788 (train) + 0 (query) + 0 (gallery)
+    """
+    dataset_dir = 'sogo'
+
+    def __init__(self, cfg, **kwargs):
+        root = cfg.DATASET.TRAIN_PATH
+        self.dataset_dir = osp.join(root, self.dataset_dir)
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
+
+        self._check_before_run()
+
+        train, num_train_pids, num_train_imgs = self._process_dir(self.train_dir, relabel=True)
+        query, num_query_pids, num_query_imgs = self._process_dir(self.query_dir, relabel=False)
+        gallery, num_gallery_pids, num_gallery_imgs = self._process_dir(self.gallery_dir, relabel=False)
+        num_total_pids = num_train_pids + num_query_pids
+        num_total_imgs = num_train_imgs + num_query_imgs + num_gallery_imgs
+
+        logger.info("=> Market1501 loaded")
+        logger.info("Dataset statistics:")
+        logger.info("  ------------------------------")
+        logger.info("  subset   | # ids | # images")
+        logger.info("  ------------------------------")
+        logger.info("  train    | {:5d} | {:8d}".format(num_train_pids, num_train_imgs))
+        logger.info("  query    | {:5d} | {:8d}".format(num_query_pids, num_query_imgs))
+        logger.info("  gallery  | {:5d} | {:8d}".format(num_gallery_pids, num_gallery_imgs))
+        logger.info("  ------------------------------")
+        logger.info("  total    | {:5d} | {:8d}".format(num_total_pids, num_total_imgs))
+        logger.info("  ------------------------------")
+
+        self.train = train
+        self.query = query
+        self.gallery = gallery
+
+        self.num_train_pids = num_train_pids
+        self.num_query_pids = num_query_pids
+        self.num_gallery_pids = num_gallery_pids
+
+    def _check_before_run(self):
+        """Check if all files are available before going deeper"""
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+
+    def _process_dir(self, dir_path, relabel=False):
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        pid_container = set()
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid:label for label, pid in enumerate(pid_container)}
+
+        dataset = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            camid -= 1 # index starts from 0
+            if relabel: pid = pid2label[pid]
+            dataset.append((img_path, pid, camid))
+
+        num_pids = len(pid_container)
+        num_imgs = len(dataset)
+        return dataset, num_pids, num_imgs
+
 class PAR():
     def __init__(self, cfg):
         self.dataset_dir = cfg.DATASET.TRAIN_PATH        
@@ -31,7 +109,7 @@ class PAR():
 
         self.cat = cfg.PAR.SELECT_CAT
         self.ignore = cfg.PAR.IGNORE_CAT
-        
+
         temp = []
         for i, a in enumerate(self.category_names, 1):
             if i not in self.ignore:
@@ -1439,7 +1517,8 @@ __img_factory = {
     'dukemtmcreid': DukeMTMCreID,
     'msmt17': MSMT17,
     'msmt17_total': MSMT17_TOTAL,
-    'par': PAR
+    'par': PAR,
+    'sogo': SOGO
 }
 
 __vid_factory = {
