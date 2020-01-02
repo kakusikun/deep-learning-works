17c62022bb9c2ea28da2d4ca3dafe06c622b3394
allenfutaki <oo24514@gmail.com>
2020-01-02 10:31:34 +0800

    update normalization layer

--- model/OSNet_PFPN.py
+++ model/OSNet_PFPN.py
@@ -21,13 +21,19 @@ from inplace_abn import InPlaceABN as IABN
 class ConvLayer(nn.Module):
     """Convolution layer (conv + bn + elu)."""
     
-    def __init__(self, name, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1):
+    def __init__(self, name, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, Norm='BN'):
         super(ConvLayer, self).__init__()
         self.g_name = name
-        self.conv = [
-            conv_iabn_lrelu(name + '/conv', in_channels, out_channels, kernel_size, stride=stride,
-                              padding=padding, groups=groups)
-        ]
+        if Norm == 'ABN':
+            self.conv = [
+                conv_iabn_lrelu(name + '/conv', in_channels, out_channels, kernel_size, stride=stride,
+                                padding=padding, groups=groups)
+            ]        
+        else:
+            self.conv = [
+                conv_bn_relu(name + '/conv', in_channels, out_channels, kernel_size, stride=stride,
+                                padding=padding, groups=groups)
+            ]
         self.conv = nn.Sequential(*self.conv)
 
     def generate_caffe_prototxt(self, caffe_net, layer):
@@ -42,12 +48,17 @@ class ConvLayer(nn.Module):
 class Conv1x1(nn.Module):
     """1x1 convolution + bn + elu."""
     
-    def __init__(self, name, in_channels, out_channels, stride=1, groups=1):
+    def __init__(self, name, in_channels, out_channels, stride=1, groups=1, Norm='BN'):
         super(Conv1x1, self).__init__()
         self.g_name = name
-        self.conv = [
-            conv_iabn_lrelu(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0, groups=groups)
-        ]
+        if Norm == 'ABN':
+            self.conv = [
+                conv_iabn_lrelu(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0, groups=groups)
+            ]
+        else:
+            self.conv = [
+                conv_bn_relu(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0, groups=groups)
+            ]
         self.conv = nn.Sequential(*self.conv)
 
     def generate_caffe_prototxt(self, caffe_net, layer):
@@ -61,12 +72,17 @@ class Conv1x1(nn.Module):
 class Conv1x1Linear(nn.Module):
     """1x1 convolution + bn (w/o non-linearity)."""
     
-    def __init__(self, name, in_channels, out_channels, stride=1):
+    def __init__(self, name, in_channels, out_channels, stride=1, Norm='BN'):
         super(Conv1x1Linear, self).__init__()
         self.g_name = name
-        self.conv = [
-            conv_iabn(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0)
-        ]
+        if Norm == 'ABN':
+            self.conv = [
+                conv_iabn(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0)
+            ]
+        else:
+            self.conv = [
+                conv_bn(name + '/1x1', in_channels, out_channels, 1, stride=stride, padding=0)
+            ]
         self.conv = nn.Sequential(*self.conv)
 
     def generate_caffe_prototxt(self, caffe_net, layer):
@@ -80,12 +96,17 @@ class Conv1x1Linear(nn.Module):
 class Conv3x3(nn.Module):
     """3x3 convolution + bn + elu."""
     
-    def __init__(self, name, in_channels, out_channels, stride=1, groups=1):
+    def __init__(self, name, in_channels, out_channels, stride=1, groups=1, Norm='BN'):
         super(Conv3x3, self).__init__()
         self.g_name = name
-        self.conv = [
-            conv_iabn_lrelu(name + '/3x3', in_channels, out_channels, 3, stride=stride, padding=1, groups=groups)
-        ]
+        if Norm == 'ABN':
+            self.conv = [
+                conv_iabn_lrelu(name + '/3x3', in_channels, out_channels, 3, stride=stride, padding=1, groups=groups)
+            ]
+        else:
+            self.conv = [
+                conv_bn_relu(name + '/3x3', in_channels, out_channels, 3, stride=stride, padding=1, groups=groups)
+            ]
         self.conv = nn.Sequential(*self.conv)
 
     def generate_caffe_prototxt(self, caffe_net, layer):
@@ -101,14 +122,19 @@ class LightConv3x3(nn.Module):
     1x1 (linear) + dw 3x3 (nonlinear).
     """
     
-    def __init__(self, name, in_channels, out_channels):
+    def __init__(self, name, in_channels, out_channels, Norm='BN'):
         super(LightConv3x3, self).__init__()
         self.g_name = name
-        self.conv = nn.Sequential(
+        self.conv = [
             g_name(name + '/pt', nn.Conv2d(in_channels, out_channels, 1, stride=1, padding=0, bias=False)),
             g_name(name + '/dw', nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, bias=False, groups=out_channels)),
-            g_name(name + '/bn', IABN(out_channels)),
-        )    
+        ]
+        if Norm == 'ABN':
+            self.conv.append(g_name(name + '/bn', IABN(out_channels)))
+        else:
+            self.conv.append(g_name(name + '/bn', nn.BatchNorm2d(out_channels)))
+
+        self.conv = nn.Sequential(*self.conv)
 
     def generate_caffe_prototxt(self, caffe_net, layer):
         layer = generate_caffe_prototxt(self.conv, caffe_net, layer)
@@ -162,36 +188,42 @@ class ChannelGate(nn.Module):
 class OSBlock(nn.Module):
     """Omni-scale feature learning block."""
     
-    def __init__(self, name, in_channels, out_channels, bottleneck_reduction=4, **kwargs):
+    def __init__(self, name, in_channels, out_channels, bottleneck_reduction=4, Norm='BN'):
         super(OSBlock, self).__init__()
         self.g_name = name
         mid_channels = out_channels // bottleneck_reduction
-        self.conv1 = Conv1x1(name + '/conv1', in_channels, mid_channels)
-        self.conv2a = LightConv3x3(name + '/conv2a/1', mid_channels, mid_channels)
+        self.conv1 = Conv1x1(name + '/conv1', in_channels, mid_channels, Norm=Norm)
+        self.conv2a = LightConv3x3(name + '/conv2a/1', mid_channels, mid_channels, Norm=Norm)
         self.conv2b = nn.Sequential(
-            LightConv3x3(name + '/conv2b/1', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2b/2', mid_channels, mid_channels),
+            LightConv3x3(name + '/conv2b/1', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2b/2', mid_channels, mid_channels, Norm=Norm),
         )
         self.conv2c = nn.Sequential(
-            LightConv3x3(name + '/conv2c/1', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2c/2', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2c/3', mid_channels, mid_channels),
+            LightConv3x3(name + '/conv2c/1', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2c/2', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2c/3', mid_channels, mid_channels, Norm=Norm),
         )
         self.conv2d = nn.Sequential(
-            LightConv3x3(name + '/conv2d/1', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2d/2', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2d/3', mid_channels, mid_channels),
-            LightConv3x3(name + '/conv2d/4', mid_channels, mid_channels),
+            LightConv3x3(name + '/conv2d/1', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2d/2', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2d/3', mid_channels, mid_channels, Norm=Norm),
+            LightConv3x3(name + '/conv2d/4', mid_channels, mid_channels, Norm=Norm),
         )
         self.gate = ChannelGate(name + '/2a/gate', mid_channels)
-        self.conv3 = Conv1x1Linear(name + '/conv3', mid_channels, out_channels)
+        self.conv3 = Conv1x1Linear(name + '/conv3', mid_channels, out_channels, Norm=Norm)
         self.downsample = None
         if in_channels != out_channels:
-            self.downsample = Conv1x1Linear(name + '/downsample', in_channels, out_channels)
+            self.downsample = Conv1x1Linear(name + '/downsample', in_channels, out_channels, Norm=Norm)
 
-        self.lrelu = nn.Sequential(
-            g_name(name + '/leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)),
-        )
+        if Norm == 'ABN':
+            self.activation = nn.Sequential(
+                g_name(name + '/leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)),
+            )
+        else:
+            self.activation = nn.Sequential(
+                g_name(name + '/relu', nn.ReLU(inplace=True)),
+            )
+
     def forward(self, x):
         residual = x
         x1 = self.conv1(x)
@@ -205,7 +237,7 @@ class OSBlock(nn.Module):
             residual = self.downsample(residual)
         out = x3 + residual
         
-        return self.lrelu(out)
+        return self.activation(out)
     
     def generate_caffe_prototxt(self, caffe_net, layer):
         gate1 = self._copy_gate(self.gate, '2b')
@@ -240,7 +272,7 @@ class OSBlock(nn.Module):
         layer = L.Eltwise(residual_layer, layer_gate_add, operation=P.Eltwise.SUM)
         caffe_net[self.g_name + '/add'] = layer
 
-        layer = generate_caffe_prototxt(self.lrelu, caffe_net, layer)
+        layer = generate_caffe_prototxt(self.activation, caffe_net, layer)
         return layer
 
     def _copy_gate(self, target, name):
@@ -256,14 +288,20 @@ class OSBlock(nn.Module):
 ##########
 
 class Regressor(nn.Module):
-    def __init__(self, name, in_channels, out_channels):
+    def __init__(self, name, in_channels, out_channels, Norm='BN'):
         super(Regressor, self).__init__()
         self.g_name = name
-        self.regressor = nn.Sequential(
+        self.regressor = [
             g_name(name + '/conv1', nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1, bias=False, groups=1)),
-            g_name(name + '/leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)),
-            g_name(name + '/conv2', nn.Conv2d(out_channels, out_channels, 1, stride=1, padding=0, bias=False, groups=1)),
-        )
+        ]
+        if Norm == 'ABN':
+            self.regressor.append(g_name(name + '/leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)))
+        else:
+            self.regressor.append(g_name(name + '/relu', nn.ReLU(inplace=True)))
+        
+        self.regressor.append(g_name(name + '/conv2', nn.Conv2d(out_channels, out_channels, 1, stride=1, padding=0, bias=False, groups=1)))
+
+        self.regressor = nn.Sequential(*self.regressor)
     
     def forward(self, x):
         x = self.regressor(x)
@@ -284,7 +322,7 @@ class OSNet(nn.Module):
           https://arxiv.org/abs/1905.00953
     """
 
-    def __init__(self, name, num_classes, blocks, layers, channels, feature_dim=512, task='object', num_keypoints=0, **kwargs):
+    def __init__(self, name, num_classes, blocks, layers, channels, feature_dim=512, task='object', num_keypoints=0, Norm='BN'):
         super(OSNet, self).__init__()
         self.g_name = name
         self.task = task
@@ -294,54 +332,54 @@ class OSNet(nn.Module):
 
         # convolutional backbone
         self.conv = nn.Sequential(
-            ConvLayer(name + '/block0', 3, channels[0], 7, stride=2, padding=3),
+            ConvLayer(name + '/block0', 3, channels[0], 7, stride=2, padding=3, Norm=Norm),
             g_name(name + '/block0/reduce/max', nn.MaxPool2d(3, stride=2, padding=1)),            
         )
 
-        self.stage1 = self._make_layer(name + '/block1', blocks[0], layers[0], channels[0], channels[1], reduce_spatial_size=True)
-        self.stage2 = self._make_layer(name + '/block2', blocks[1], layers[1], channels[1], channels[2], reduce_spatial_size=True)
-        self.stage3 = self._make_layer(name + '/block3', blocks[2], layers[2], channels[2], channels[3], reduce_spatial_size=False)
+        self.stage1 = self._make_layer(name + '/block1', blocks[0], layers[0], channels[0], channels[1], reduce_spatial_size=True, Norm=Norm)
+        self.stage2 = self._make_layer(name + '/block2', blocks[1], layers[1], channels[1], channels[2], reduce_spatial_size=True, Norm=Norm)
+        self.stage3 = self._make_layer(name + '/block3', blocks[2], layers[2], channels[2], channels[3], reduce_spatial_size=False, Norm=Norm)
 
-        self.fpn1 = Conv1x1(self.g_name + '/fpn1', channels[1], 256)
-        self.fpn2 = Conv1x1(self.g_name + '/fpn2', channels[2], 256)
-        self.fpn3 = Conv1x1(self.g_name + '/fpn3', channels[3], 256)
+        self.fpn1 = Conv1x1(self.g_name + '/fpn1', channels[1], 256, Norm=Norm)
+        self.fpn2 = Conv1x1(self.g_name + '/fpn2', channels[2], 256, Norm=Norm)
+        self.fpn3 = Conv1x1(self.g_name + '/fpn3', channels[3], 256, Norm=Norm)
         self.fpn3_upsample = upsample(self.g_name + '/fpn3/upsample' , scale_factor=2, in_channels=256)
         self.fpn2_upsample = upsample(self.g_name + '/fpn2/upsample' , scale_factor=2, in_channels=256)
 
-        self.seg1 = Conv3x3(self.g_name + '/seg1', 256, 128)
+        self.seg1 = Conv3x3(self.g_name + '/seg1', 256, 128, Norm=Norm)
         self.seg1_upsample   = upsample(self.g_name + '/seg1/upsample'   , scale_factor=2, in_channels=128)
-        self.seg2_1 = Conv3x3(self.g_name + '/seg2_1', 256, 128)
-        self.seg2_2 = Conv3x3(self.g_name + '/seg2_2', 128, 128)
+        self.seg2_1 = Conv3x3(self.g_name + '/seg2_1', 256, 128, Norm=Norm)
+        self.seg2_2 = Conv3x3(self.g_name + '/seg2_2', 128, 128, Norm=Norm)
         self.seg2_1_upsample = upsample(self.g_name + '/seg2_1/upsample'   , scale_factor=2, in_channels=128)
         self.seg2_2_upsample = upsample(self.g_name + '/seg2_2/upsample'   , scale_factor=2, in_channels=128)
-        self.seg3_1 = Conv3x3(self.g_name + '/seg3_1', 256, 128)
-        self.seg3_2 = Conv3x3(self.g_name + '/seg3_2', 128, 128)
+        self.seg3_1 = Conv3x3(self.g_name + '/seg3_1', 256, 128, Norm=Norm)
+        self.seg3_2 = Conv3x3(self.g_name + '/seg3_2', 128, 128, Norm=Norm)
         self.seg3_1_upsample = upsample(self.g_name + '/seg3_1/upsample' , scale_factor=2, in_channels=128)
         self.seg3_2_upsample = upsample(self.g_name + '/seg3_2/upsample' , scale_factor=2, in_channels=128)    
 
-        self.hm_reg     = Regressor(self.g_name + '/object_heatmap', 128, num_classes)
-        self.offset_reg = Regressor(self.g_name + '/object_offset', 128, 2)
-        self.size_reg   = Regressor(self.g_name + '/object_size', 128, 2)
+        self.hm         = Regressor(self.g_name + '/object_heatmap', 128, num_classes, Norm=Norm)
+        self.offset_reg = Regressor(self.g_name + '/object_offset', 128, 2, Norm=Norm)
+        self.size_reg   = Regressor(self.g_name + '/object_size', 128, 2, Norm=Norm)
 
         if task == 'keypoint':
             assert num_keypoints != 0
-            self.kp_hm_reg     = Regressor(self.g_name + '/keypoint_heatmap', 128, num_keypoints)
-            self.kp_offset_reg = Regressor(self.g_name + '/keypoint_offset', 128, 2)
-            self.kp_loc_reg    = Regressor(self.g_name + '/keypoint_location', 128, num_keypoints * 2)
+            self.kp_hm         = Regressor(self.g_name + '/keypoint_heatmap', 128, num_keypoints, Norm=Norm)
+            self.kp_offset_reg = Regressor(self.g_name + '/keypoint_offset', 128, 2, Norm=Norm)
+            self.kp_loc_reg    = Regressor(self.g_name + '/keypoint_location', 128, num_keypoints * 2, Norm=Norm)
 
         self._init_params()
 
-    def _make_layer(self, name, block, layer, in_channels, out_channels, reduce_spatial_size):
+    def _make_layer(self, name, block, layer, in_channels, out_channels, reduce_spatial_size, Norm='BN'):
         layers = []
         
-        layers.append(block(name + '/0', in_channels, out_channels))
+        layers.append(block(name + '/0', in_channels, out_channels, Norm=Norm))
         for i in range(1, layer):
-            layers.append(block(name + '/{}'.format(i), out_channels, out_channels))
+            layers.append(block(name + '/{}'.format(i), out_channels, out_channels, Norm=Norm))
         
         if reduce_spatial_size:
             layers.append(
                 nn.Sequential(
-                    Conv1x1(name + '/reduce', out_channels, out_channels),
+                    Conv1x1(name + '/reduce', out_channels, out_channels, Norm=Norm),
                     g_name(name + '/reduce/avg', nn.AvgPool2d(2, stride=2))
                 )
             )
@@ -387,12 +425,12 @@ class OSNet(nn.Module):
         seg2_2 = self.seg2_2_upsample(self.seg2_2(seg2_1)) + seg3_2
         seg1   = self.seg1_upsample(self.seg1(fpn1)) + seg2_2
 
-        ob_hm     = self.hm_reg(seg1)
+        ob_hm     = self.hm(seg1)
         ob_offset = self.offset_reg(seg1)
         ob_size   = self.size_reg(seg1)
 
         if self.task == 'keypoint':
-            kp_hm = self.kp_hm_reg(seg1)
+            kp_hm = self.kp_hm(seg1)
             kp_loc = self.kp_loc_reg(seg1)
             kp_offset = self.kp_offset_reg(seg1)
             return ob_hm, ob_offset, ob_size, kp_hm, kp_offset, kp_loc
@@ -447,7 +485,14 @@ class OSNet(nn.Module):
 ##########
 # Instantiation
 ##########
-def osnet_x1_0(name, num_classes=1000, **kwargs):
-    # standard size (width x1.0)
-    return OSNet(name, num_classes, blocks=[OSBlock, OSBlock, OSBlock], layers=[2, 2, 2],
-                 channels=[64, 256, 384, 512], **kwargs)
+def get_osnet_pfpn(cfg):
+    model = OSNet(cfg.MODEL.NAME, 
+                  cfg.DB.NUM_CLASSES, 
+                  blocks=[OSBlock, OSBlock, OSBlock], 
+                  layers=[2, 2, 2],
+                  channels=[64, 256, 384, 512], 
+                  feature_dim=512, 
+                  task=cfg.TASK, 
+                  num_keypoints=cfg.DB.NUM_KEYPOINTS*2, 
+                  Norm=cfg.MODEL.NORM)
+    return model
