0c274d4edba2f7c783655c4708604b1010ed2862
allenfutaki <oo24514@gmail.com>
2020-02-27 20:39:33 +0800

    TODO

--- src/factory/backbone_factory.py
+++ src/factory/backbone_factory.py
@@ -1,6 +1,7 @@
 from src.base_factory import BaseFactory
 from src.model.backbone import osnet
 
+#TODO: remove BaseFactory
 class BackboneFactory(BaseFactory):
     products = {
         'osnet': osnet
--- src/factory/graph_factory.py
+++ src/factory/graph_factory.py
@@ -1,6 +1,7 @@
 from src.base_factory import BaseFactory
 from src.graph.simple_classifier import SimpleClassifier
 
+#TODO: remove BaseFactory
 class GraphFactory(BaseFactory):
     products = {
         'simple_classifier': SimpleClassifier,
