cd0b693613558436a6e19cc6fc2b1f5232fe8e67
allenfutaki <oo24514@gmail.com>
2020-03-01 23:24:32 +0800

    add reshape head for object detection

--- src/model/module/head_module.py
+++ src/model/module/head_module.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class ClassificationHead(nn.Module):
     def __init__(self, in_channels, num_classes):
-        super().__init__()
+        super(ClassificationHead, self).__init__()
         self.gap = nn.AdaptiveAvgPool2d(1)        
         self.dropout = nn.Dropout(0.2)
         self.classifier = nn.Linear(in_channels, num_classes)   
@@ -15,3 +15,15 @@ class ClassificationHead(nn.Module):
         x = self.classifier(x)
         return x
 
+class ReshapeHead(nn.Module):
+    def __init__(self, in_channels, num_dim):   
+        super(ReshapeHead, self).__init__()
+        self.reshape = nn.Conv2d(
+            in_channels,
+            num_dim,
+            1,
+            stride=1,
+            padding=0,
+            bias=True)
+    def forward(self, x):
+        return self.reshape(x)
