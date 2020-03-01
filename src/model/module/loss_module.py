1b2319cc8017aa0af11edf783ff5ae0feb537bc3
allenfutaki <oo24514@gmail.com>
2020-03-01 23:25:25 +0800

    add loss module with smooth l1 loss and focal loss

---
+++ src/model/module/loss_module.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tools.utils import _tranpose_and_gather_feat
+
+class SmoothL1Loss(nn.Module):
+    def __init__(self):
+        super(SmoothL1Loss, self).__init__()
+    
+    def forward(self, output, mask, ind, target):
+        pred = _tranpose_and_gather_feat(output, ind)
+        mask = mask.unsqueeze(2).expand_as(pred).float()
+        loss = F.smooth_l1_loss(pred * mask, target * mask, reduction='sum')
+        loss = loss / (mask.sum() + 1e-4)
+        return loss
+
+class FocalLoss(nn.Module):
+    ''' Modified focal loss. Exactly the same as CornerNet.
+        Runs faster and costs a little bit more memory
+      Arguments:
+        pred (batch x c x h x w)
+        gt_regr (batch x c x h x w)
+    '''
+    def __init__(self, a=2, b=4):
+        super(FocalLoss, self).__init__()
+        self.a = a
+        self.b = b
+    def forward(self, loss_w, feat, target):
+        pos_inds = target.eq(1).float()
+        neg_inds = target.lt(1).float()
+
+        pos_loss = pos_inds * torch.exp(-1.0 * loss_w[0]) * torch.log(feat) * torch.pow(1 - feat, self.a)
+        neg_loss = neg_inds * torch.exp(-1.0 * loss_w[1]) * torch.pow(1 - target, self.b) * torch.pow(feat, self.a) * torch.log(1 - feat)
+    
+        num_pos  = pos_inds.float().sum()
+        pos_loss = pos_loss.sum()
+        neg_loss = neg_loss.sum()
+    
+        if num_pos == 0:
+            return -neg_loss
+        else:
+            return -1.0 * (pos_loss + neg_loss) / num_pos
