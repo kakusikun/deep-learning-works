c6ff3414ad2fc5f8ce511cbe23061acdfca5097a
allenfutaki <oo24514@gmail.com>
2020-03-03 23:38:56 +0800

    add regression head

--- src/model/module/head_module.py
+++ src/model/module/head_module.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from src.model.module.base_module import ConvModule
 
-class ClassificationHead(nn.Module):
+class ClassifierHead(nn.Module):
     def __init__(self, in_channels, num_classes):
-        super(ClassificationHead, self).__init__()
+        super(ClassifierHead, self).__init__()
         self.gap = nn.AdaptiveAvgPool2d(1)        
         self.dropout = nn.Dropout(0.2)
         self.classifier = nn.Linear(in_channels, num_classes)   
@@ -15,15 +16,27 @@ class ClassificationHead(nn.Module):
         x = self.classifier(x)
         return x
 
-class ReshapeHead(nn.Module):
-    def __init__(self, in_channels, num_dim):   
-        super(ReshapeHead, self).__init__()
-        self.reshape = nn.Conv2d(
-            in_channels,
-            num_dim,
-            1,
-            stride=1,
-            padding=0,
-            bias=True)
+class RegressionHead(nn.Module):
+    def __init__(self, in_channels, num_dim, num_layers=4, feat_size=64):   
+        super(RegressionHead, self).__init__()
+        features = []
+        for _ in range(num_layers):
+            features.append(
+                ConvModule(
+                    in_channels,
+                    feat_size,
+                    3,
+                    stride=1,
+                    padding=1,
+                    activation='relu',
+                    use_bn=False
+                )
+            )
+            in_channels = feat_size
+        self.features = nn.Sequential(*features)
+        self.head = ConvModule(
+            in_channels, num_dim, 3, stride=1, padding=1, activation='linear', use_bn=False)
     def forward(self, x):
-        return self.reshape(x)
+        x = self.features(x)
+        x = self.head(x)
+        return x
