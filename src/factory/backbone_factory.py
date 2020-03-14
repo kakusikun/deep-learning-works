4416984d801f61e30df6b2cf85206db3fe445991
allenfutaki <oo24514@gmail.com>
2020-03-14 17:31:53 +0800

    add shufflenetv2 from torchvision to use pretrained weight

--- src/factory/backbone_factory.py
+++ src/factory/backbone_factory.py
@@ -1,11 +1,13 @@
 from src.model.backbone.osnet import osnet
 from src.model.backbone.shufflenetv2_plus import shufflenetv2_plus
 from src.model.backbone.hourglass import hourglass_net
+from src.model.backbone.shufflenetv2_torchvision import shufflenetv2
 class BackboneFactory:
     products = {
         'osnet': osnet,
         'shufflenetv2+': shufflenetv2_plus,
         'hourglass': hourglass_net,
+        'shufflenetv2': shufflenetv2,
     }
 
     @classmethod
