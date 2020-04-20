b5a8ee165346ed6933484cb8804232645fc293f5
allenfutaki <oo24514@gmail.com>
2020-04-20 10:27:27 +0800

    update

--- src/database/data/__init__.py
+++ src/database/data/__init__.py
@@ -3,7 +3,11 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import lmdb
+try:
+    import lmdb
+except:
+    print("lmdb is not installed")
+    
 from tqdm import tqdm 
 from src.base_data import BaseData
 import logging
