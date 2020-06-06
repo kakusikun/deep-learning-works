5723488f0bff88575273dbc0a518ae67c418226e
kakusikun <oo24514@gmail.com>
2020-06-06 07:49:13 +0000

    TODO: CSP Head

--- src/model/module/head_module.py
+++ src/model/module/head_module.py
@@ -298,3 +298,33 @@ class AMSoftmaxClassiferHeadForJDE(nn.Module):
         x = _tranpose_and_gather_feat(x, ind)[mask > 0]
         cosine = F.linear(x, F.normalize(self.weight)).clamp(-1, 1)
         return cosine
+
+class L2Norm(nn.Module):
+    def __init__(self, n_channels, scale):
+        super(L2Norm, self).__init__()
+        self.n_channels = n_channels
+        self.gamma = scale or None
+        self.eps = 1e-10
+        self.weight = nn.Parameter(torch.Tensor(self.n_channels))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.constant_(self.weight, self.gamma)
+
+    def forward(self, x):
+        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+self.eps
+        x = torch.div(x, norm)
+        out = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x) * x
+        return out
+
+# TODO: use CSP
+# class CSPHead(nn.Module):
+#     def __init__(self, num_stages):
+#         super(CSPHead, self).__init__()
+#         self.num_stages = num_stages
+#         pks = nn.ModuleList()
+#         for _ in range(self.num_stages):
+#             pks.append(nn.Sequential(
+#                 nn.ConvTranspose2d
+#             ))
+
